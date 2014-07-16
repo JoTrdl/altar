43865f1e7683dc1431a5f119ed3d1f3d0c6724b0
Johann Troendle <johann.troendle@gmail.com>
2014-07-16 13:55:29 -0400

    Added error route

---
+++ app/controllers/web/error-controller.js
@@ -0,0 +1,5 @@
+
+exports.notFound = function(req, res, next){
+  res.status(404);
+  res.render('errors/404');
+};
--- app/routes/web.js
+++ app/routes/web.js
@@ -1,3 +1,6 @@
 
 app.get('/', controllers.web.homeController.doGet);
 app.post('/', controllers.web.homeController.doPost);
+
+
+app.get('*', controllers.web.errorController.notFound);
