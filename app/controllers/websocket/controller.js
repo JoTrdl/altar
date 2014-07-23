047cb22c2b386f2bb78150d98ad908964b528d06
Johann Troendle <johann.troendle@gmail.com>
2014-07-23 16:05:40 -0400

    async + small upgrades

--- app/controllers/websocket/controller.js
+++ app/controllers/websocket/controller.js
@@ -1,4 +1,9 @@
 
+
+exports.onConnectionEvent = function(socket) {
+  // called when a new socket connects to the server
+};
+
 exports.handleEvent = function(socket, args, next){
   var name = args.shift(), msg = args.shift();
   socket.emit('received event', name, msg);
--- app/routes/websoket.js
+++ app/routes/websoket.js
@@ -1,4 +1,5 @@
 
 
+app.io.on('connection', controllers.websocket.controller.onConnectionEvent);
 app.io.router.on('some*', controllers.websocket.controller.handleEvent);
 app.io.router.on('*event', controllers.websocket.controller.handleEvent);
