d6cb7559f5f3bd8de2d1a6efe1658f8a0e6ed535
Johann Troendle <johann.troendle@gmail.com>
2014-08-22 15:17:29 -0400

    Fixed sourcing environment file

--- config/development/config.js
+++ config/development/config.js
@@ -1,7 +1,6 @@
 
 // Node serves static files in dev
 app.use( express.static(app.settings.static) );
-app.use( require('morgan')('dev') );
 
 // Pretty HTML in dev
 app.use(function(req, res, next) {
